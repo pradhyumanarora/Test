42d74f834e30f744c846ed3102fe9a93341bcac6
Pradhyuman Arora <72353093+pradhyumanarora@users.noreply.github.com>
2022-05-28 11:49:05 +0530

    Add files via upload

---
+++ spotify/credentials.py
@@ -0,0 +1,7 @@
+from http import client
+
+
+CLIENT_ID = "3a1ae0eba83945f0ada9b1ec5fa1543d"
+CLIENT_SECRET  = "c22cfb23428a4b5fb511dd51de323be9"
+
+REDIRECT_URI = "http://localhost:8000/spotify/redirect"   
---
+++ spotify/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from spotify.views import *
+
+app_name = 'spotify'
+
+urlpatterns = [
+    path('auth',AuthURL.as_view()),
+    path('redirect',spotify_callback),
+    path('is-authenticated', IsAuthenticated.as_view()),
+    path('print',ShowData.as_view()),
+    path('current-song',CurrentSong.as_view()),
+    path('testing',Testing.as_view())
+]
+
+# get-aut-url
